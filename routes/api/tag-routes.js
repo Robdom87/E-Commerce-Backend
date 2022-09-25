fc4582840f177ab20c91f0d5c560c0379a77ad3d
Cedar Chavez <geggas00@gmail.com>
2022-09-25 14:25:33 -0700

    add product routes and debug tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }]
+      include: [{ model: Product, through: ProductTag, as: 'T2P' }]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -20,8 +20,8 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Category.findByPk(Tag.params.id, {
-      include: [{ model: Product }]
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'T2P' }]
     });
 
     if (!tagData) {
